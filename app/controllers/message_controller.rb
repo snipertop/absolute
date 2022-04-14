12dfa1162eb695b7c4ea0a5e6b9fac03dbc98bb1
dinian <1361149082@qq.com>
2022-04-14 08:01:34 +0800

    fix-message

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -5,11 +5,26 @@ class MessageController < ApplicationController
 
     def msg
         payload = params[:data]
+        return if payload.empty?
         to = payload["to"]
-        title = payload["entry_name"]
-        content = payload["content"]
+        title = payload["notify_text"]
+        content = payload["entry_name"]
         url = payload["url"]
-        return if to.nil? and title.nil? and content.nil? and url.nil?
         send_message(to, title, content, url)
     end
 end
+
+{
+    "data"=>{
+        "content"=>"", 
+        "entry_name"=>"重置密码", 
+        "flow_action"=>"flow_forward", 
+        "notify_text"=>"有新的流程待办事项", 
+        "to"=>[{"name"=>"阮琳赈", "username"=>"1703018"}, {"name"=>"耿中宝", "username"=>"1703017"}], 
+        "url"=>"https://www.jiandaoyun.com/dashboard/app/621c17b51f2b340007faa95b/form/621c17ba218da2000858fcff/data/62576229ee502d0008487cd0?actionType=flow_forward&flowId=1&memberType=0&guestCorpId="
+        }, 
+        "op"=>"flow_message", 
+        "send_time"=>"2022-04-13T23:52:09.597Z", 
+        "nonce"=>"956f23", 
+        "timestamp"=>"1649893929", 
+        "message"=>{"data"=>{"content"=>"", "entry_name"=>"重置密码", "flow_action"=>"flow_forward", "notify_text"=>"有新的流程待办事项", "to"=>[{"name"=>"阮琳赈", "username"=>"1703018"}, {"name"=>"耿中宝", "username"=>"1703017"}], "url"=>"https://www.jiandaoyun.com/dashboard/app/621c17b51f2b340007faa95b/form/621c17ba218da2000858fcff/data/62576229ee502d0008487cd0?actionType=flow_forward&flowId=1&memberType=0&guestCorpId="}, "op"=>"flow_message", "send_time"=>"2022-04-13T23:52:09.597Z"}}
