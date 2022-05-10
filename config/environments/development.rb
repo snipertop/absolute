dad6817cebd13672686d07a62a5b0aee7cf5b85e
dinian <1361149082@qq.com>
2022-05-10 20:19:02 +0800

    wexin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.hosts << "zbu.free.svipss.top"
+  config.hosts << "xxk.zbu.edu.cn"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
